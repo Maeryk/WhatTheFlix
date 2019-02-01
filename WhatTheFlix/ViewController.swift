eb73bd95a3830c946dcf3cdb98afe950ce0835ea
GORDON BITTNER <1007806253@wce111-16.local>
2019-01-31 18:40:20 -0800

    Initial Commit

---
+++ WhatTheFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatTheFlix
+//
+//  Created by GORDON BITTNER on 1/31/19.
+//  Copyright © 2019 GORDON BITTNER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
