d0e2f031791d29265f357567a89a745b254e46d5
GORDON BITTNER <1007806253@wce111-16.local>
2019-02-07 20:22:16 -0800

    Part 2

---
+++ WhatTheFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  WhatTheFlix
+//
+//  Created by GORDON BITTNER on 2/7/19.
+//  Copyright © 2019 GORDON BITTNER. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- WhatTheFlix/MoviesViewController.swift
+++ WhatTheFlix/MoviesViewController.swift
@@ -77,5 +77,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     }
     */
+    override func prepare(for segue:
+        UIStoryboardSegue, sender: Any?){
+        
+        //find selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //pass selected movie to the details view controller
+        let detailsViewController = segue.destination as! MoviesDetailViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 
 }
